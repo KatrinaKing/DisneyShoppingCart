0806740dba4bfd7b89666e661fbbed93516d9855
Kat <katrinaking25@gmail.com>
2021-05-14 20:21:57 -0600

    created a method and the answer printed out in console

---
+++ DisneyShoppingCart/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DisneyShoppingCart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 6384;
+            int b = 73893;
+            bool isItTrue = false;
+
+            isItTrue = a != b;
+            Console.WriteLine(isItTrue);
+        }
+    }
+}
