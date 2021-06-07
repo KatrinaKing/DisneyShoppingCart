b96a43de94f422c923ca34008eaf71c807715de6
Kat <katrinaking25@gmail.com>
2021-06-07 00:25:23 -0600

    updated

--- DisneyShoppingCart/Repositories/Implementation/UserRepository.cs
+++ DisneyShoppingCart/Repositories/Implementation/UserRepository.cs
@@ -14,7 +14,7 @@ namespace DisneyShoppingCart.Repositories.Implementation
 
         
 
-        public void GetAllUsersByName(string username)
+        public void GetAllUsersByName(string UserName)
         {
             List<Users> resultList =
                 currentDataBase.Where(Users => Users.UserName == "UserName").ToList();
